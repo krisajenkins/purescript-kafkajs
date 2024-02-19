fd4718cabd1052df99ced0f39a0680ef8746fc28
Kris Jenkins <krisajenkins@gmail.com>
2024-02-19 07:18:36 -0500

    Adding derived Eq and Ord instances for Offset/Partition/Topic.
    The immediate need here is using Topic as a Map key.

--- src/Kafka/Types.purs
+++ src/Kafka/Types.purs
@@ -4,7 +4,9 @@ module Kafka.Types
   , Topic(..)
   ) where
 
+import Data.Eq (class Eq)
 import Data.Newtype (class Newtype)
+import Data.Ord (class Ord)
 import Data.Show (class Show)
 
 newtype Offset
@@ -14,6 +16,10 @@ instance ntOffset :: Newtype Offset Int
 
 derive newtype instance showOffset :: Show Offset
 
+derive newtype instance eqOffset :: Eq Offset
+
+derive newtype instance ordOffset :: Ord Offset
+
 newtype Partition
   = Partition Int
 
@@ -21,9 +27,17 @@ instance ntPartiton :: Newtype Partition Int
 
 derive newtype instance showPartition :: Show Partition
 
+derive newtype instance eqPartition :: Eq Partition
+
+derive newtype instance ordPartition :: Ord Partition
+
 newtype Topic
   = Topic String
 
 instance ntTopic :: Newtype Topic String
 
 derive newtype instance showTopic :: Show Topic
+
+derive newtype instance eqTopic :: Eq Topic
+
+derive newtype instance ordTopic :: Ord Topic
